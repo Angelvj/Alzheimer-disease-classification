74e337b056f531794d547cc96b060a8c19e62521
Ángel de la Vega Jiménez <32851181+Angelvj@users.noreply.github.com>
2021-06-08 14:57:45 +0200

    added configuration file

---
+++ code/config.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  8 14:49:02 2021
+
+@author: angel
+"""
+
+import tensorflow as tf
+
+AUTO = None
+REPLICAS = None
+STRATEGY = None
+
+
+def initialize_device(device):
+    
+    global AUTO, REPLICAS, STRATEGY
+    
+    if device == "TPU":
+        try:
+            tpu = tf.distribute.cluster_resolver.TPUClusterResolver()
+            tf.config.experimental_connect_to_cluster(tpu)
+            tf.tpu.experimental.initialize_tpu_system(tpu)
+            STRATEGY = tf.distribute.experimental.TPUStrategy(tpu)
+        except ValueError:
+            print('Could not connect to TPU, setting default strategy')
+            tpu = None
+            STRATEGY = tf.distribute.get_strategy()
+            
+    elif device == "GPU":
+        STRATEGY = tf.distribute.MirroredStrategy()
+        
+
+    AUTO     = tf.data.experimental.AUTOTUNE
+    REPLICAS = STRATEGY.num_replicas_in_sync
+    print(f'Number of accelerators: {REPLICAS}')
