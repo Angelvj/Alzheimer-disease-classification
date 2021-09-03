bb6320ca0d1123229d5b6af1913ea5a9fc82d1c5
Ángel de la Vega Jiménez <32851181+Angelvj@users.noreply.github.com>
2021-09-03 09:25:26 +0200

    Added resnet in python code

---
+++ code/models/ResNet.py
@@ -0,0 +1,264 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  3 09:16:12 2021
+
+@author: angel
+"""
+
+import tensorflow as tf
+import six
+import tensorflow.keras.backend as K
+from math import ceil
+from tensorflow.keras.layers import MaxPooling3D, Dense, Conv3D, BatchNormalization, Input, GlobalAveragePooling3D, add, Activation
+from tensorflow.keras.regularizers import l2
+
+pet_shape = (79, 95, 68, 1)
+mri_shape = (121, 145, 121, 1)
+mri_shape_downsample = (75, 90, 75, 1)
+
+# This code is an adaptation of https://github.com/raghakot/keras-resnet/blob/master/resnet.py 
+# to 3D
+def _bn_relu(input):
+    """Helper to build a BN -> relu block
+    """
+    norm = BatchNormalization(axis=CHANNEL_AXIS)(input)
+    return Activation("relu")(norm)
+
+def _conv_bn_relu(**conv_params):
+    """Helper to build a conv -> BN -> relu block
+    """
+    filters = conv_params["filters"]
+    kernel_size = conv_params["kernel_size"]
+    strides = conv_params.setdefault("strides", (1, 1, 1))
+    kernel_initializer = conv_params.setdefault(
+        "kernel_initializer", "he_normal")
+    padding = conv_params.setdefault("padding", "same")
+    kernel_regularizer = conv_params.setdefault("kernel_regularizer", l2(1e-4))
+
+    def f(input):
+        conv = Conv3D(filters=filters, kernel_size=kernel_size,
+                      strides=strides, kernel_initializer=kernel_initializer,
+                      padding=padding,
+                      kernel_regularizer=kernel_regularizer)(input)
+        return _bn_relu(conv)
+
+    return f
+
+
+def _bn_relu_conv(**conv_params):
+    """Helper to build a BN -> relu -> conv block.
+    This is an improved scheme proposed in http://arxiv.org/pdf/1603.05027v2.pdf
+    """
+    filters = conv_params["filters"]
+    kernel_size = conv_params["kernel_size"]
+    strides = conv_params.setdefault("strides", (1, 1, 1))
+    kernel_initializer = conv_params.setdefault("kernel_initializer", "he_normal")
+    padding = conv_params.setdefault("padding", "same")
+    kernel_regularizer = conv_params.setdefault("kernel_regularizer", l2(1e-4))
+
+    def f(input):
+        activation = _bn_relu(input)
+        return Conv3D(filters=filters, kernel_size=kernel_size,
+                      strides=strides, kernel_initializer=kernel_initializer,
+                      padding=padding,
+                      kernel_regularizer=kernel_regularizer)(activation)
+    return f
+
+
+def _shortcut(input, residual):
+    """Adds a shortcut between input and residual block and merges them with "sum"
+    """
+    # Expand channels of shortcut to match residual.
+    # Stride appropriately to match residual (width, height)
+    # Should be int if network architecture is correctly configured.
+    input_shape = K.int_shape(input)
+    residual_shape = K.int_shape(residual)
+    stride_width = ceil(input_shape[ROW_AXIS] / residual_shape[ROW_AXIS])
+    stride_height = ceil(input_shape[COL_AXIS] / residual_shape[COL_AXIS])
+    stride_depth = ceil(input_shape[DEPTH_AXIS] / residual_shape[DEPTH_AXIS])
+    equal_channels = input_shape[CHANNEL_AXIS] == residual_shape[CHANNEL_AXIS]
+
+    shortcut = input
+    # 1 X 1 conv if shape is different. Else identity.
+    if stride_width > 1 or stride_height > 1 or stride_depth > 1 \
+            or not equal_channels:
+        shortcut = Conv3D(
+            filters=residual_shape[CHANNEL_AXIS],
+            kernel_size=(1, 1, 1),
+            strides=(stride_width, stride_height, stride_depth),
+            kernel_initializer="he_normal", padding="valid",
+            kernel_regularizer=l2(1e-4)
+            )(input)
+    return add([shortcut, residual])
+
+
+def _residual_block(block_function, filters, kernel_regularizer, repetitions,
+                      is_first_layer=False):
+    """Builds a residual block with repeating bottleneck blocks.
+    """
+    def f(input):
+        for i in range(repetitions):
+            strides = (1, 1, 1)
+            if i == 0 and not is_first_layer:
+                strides = (2, 2, 2)
+            input = block_function(filters=filters, strides=strides,
+                                   kernel_regularizer=kernel_regularizer,
+                                   is_first_block_of_first_layer=(
+                                       is_first_layer and i == 0)
+                                   )(input)
+        return input
+
+    return f
+
+
+def basic_block(filters, strides=(1, 1, 1), kernel_regularizer=l2(1e-4),
+                is_first_block_of_first_layer=False):
+    """Basic 3 X 3 convolution blocks for use on resnets with layers <= 34.
+    Follows improved proposed scheme in http://arxiv.org/pdf/1603.05027v2.pdf
+    """
+    def f(input):
+        if is_first_block_of_first_layer:
+            # don't repeat bn->relu since we just did bn->relu->maxpool
+            conv1 = Conv3D(filters=filters, kernel_size=(3, 3, 3),
+                           strides=strides, padding="same",
+                           kernel_initializer="he_normal",
+                           kernel_regularizer=kernel_regularizer
+                           )(input)
+        else:
+            conv1 = _bn_relu_conv(filters=filters,
+                                    kernel_size=(3, 3, 3),
+                                    strides=strides,
+                                    kernel_regularizer=kernel_regularizer
+                                    )(input)
+
+        residual = _bn_relu_conv(filters=filters, kernel_size=(3, 3, 3),
+                                   kernel_regularizer=kernel_regularizer
+                                   )(conv1)
+        return _shortcut(input, residual)
+
+    return f
+
+
+def bottleneck(filters, strides=(1, 1, 1), kernel_regularizer=l2(1e-4),
+               is_first_block_of_first_layer=False):
+    """Bottleneck architecture for > 34 layer resnet.
+    Follows improved proposed scheme in http://arxiv.org/pdf/1603.05027v2.pdf
+    Returns:
+        A final conv layer of filters * 4
+    """
+    def f(input):
+        if is_first_block_of_first_layer:
+            # don't repeat bn->relu since we just did bn->relu->maxpool
+            conv_1_1 = Conv3D(filters=filters, kernel_size=(1, 1, 1),
+                              strides=strides, padding="same",
+                              kernel_initializer="he_normal",
+                              kernel_regularizer=kernel_regularizer
+                              )(input)
+        else:
+            conv_1_1 = _bn_relu_conv(filters=filters, kernel_size=(1, 1, 1),
+                                       strides=strides,
+                                       kernel_regularizer=kernel_regularizer
+                                       )(input)
+
+        conv_3_3 = _bn_relu_conv(filters=filters, kernel_size=(3, 3, 3),
+                                   kernel_regularizer=kernel_regularizer
+                                   )(conv_1_1)
+        residual = _bn_relu_conv(filters=filters * 4, kernel_size=(1, 1, 1),
+                                   kernel_regularizer=kernel_regularizer
+                                   )(conv_3_3)
+
+        return _shortcut(input, residual)
+
+    return f
+
+def _handle_data_format():
+    global ROW_AXIS
+    global COL_AXIS
+    global DEPTH_AXIS
+    global CHANNEL_AXIS
+    if K.image_data_format() == 'channels_last':
+        ROW_AXIS = 1
+        COL_AXIS = 2
+        DEPTH_AXIS = 3
+        CHANNEL_AXIS = 4
+    else:
+        CHANNEL_AXIS = 1
+        ROW_AXIS = 2
+        COL_AXIS = 3
+        DEPTH_AXIS = 4
+
+
+def _get_block(identifier):
+    if isinstance(identifier, six.string_types):
+        res = globals().get(identifier)
+        if not res:
+            raise ValueError('Invalid {}'.format(identifier))
+        return res
+    return identifier
+
+
+class ResnetBuilder(object):
+    """ResNet."""
+
+    @staticmethod
+    def build(input_shape, num_outputs, block_fn, repetitions, reg_factor):
+        """Builds a custom ResNet like architecture.
+        Args:
+            input_shape: The input shape in the form (nb_channels, nb_rows, nb_cols)
+            num_outputs: The number of outputs at final softmax layer
+            block_fn: The block function to use. This is either `basic_block` or `bottleneck`.
+                The original paper used basic_block for layers < 50
+            repetitions: Number of repetitions of various block units.
+                At each block unit, the number of filters are doubled and the input size is halved
+        Returns:
+            The keras `Model`.
+        """
+        _handle_data_format()
+        if len(input_shape) != 4:
+            raise ValueError("Input should have 4 dimensions")
+
+        # Load function from str if needed.
+        block_fn = _get_block(block_fn)
+
+        input = Input(shape=input_shape)
+        conv1 = _conv_bn_relu(filters=64, kernel_size=(7, 7, 7), strides=(2, 2, 2), kernel_regularizer=l2(reg_factor))(input)
+        pool1 = MaxPooling3D(pool_size=(3, 3, 3), strides=(2, 2, 2), padding="same")(conv1)
+
+        block = pool1
+        filters = 64
+        for i, r in enumerate(repetitions):
+            block = _residual_block(block_fn, filters=filters, kernel_regularizer=l2(reg_factor), repetitions=r, is_first_layer=(i == 0))(block)
+            filters *= 2
+
+        # last activation
+        block = _bn_relu(block)
+
+
+        # Classifier block
+        pool2 = GlobalAveragePooling3D()(block)
+        
+        # flatten1 = Flatten()(pool2)
+        if num_outputs > 1:
+            dense = Dense(units=num_outputs, kernel_initializer="he_normal", activation="softmax", kernel_regularizer=l2(reg_factor))(pool2)
+        else:
+            dense = Dense(units=num_outputs, kernel_initializer="he_normal", activation="sigmoid", kernel_regularizer=l2(reg_factor))(pool2)
+
+        model = tf.keras.Model(inputs=input, outputs=dense)
+        return model
+
+
+    @staticmethod
+    def build_resnet(num_layers, input_shape, num_outputs, reg_factor=1e-4):
+        """Build resnet 18, 34, 50, 101 or 152"""
+
+        repetitions = {18: [2, 2, 2, 2], 
+                       34: [3, 4, 6, 3],
+                       50: [3, 4, 6, 3],
+                       101: [2, 4, 23, 3],
+                       152: [3, 8, 36, 3]}
+
+        block_fn = {18: basic_block, 34: basic_block, 50: bottleneck,
+                    101: bottleneck, 152:bottleneck}
+
+        return ResnetBuilder.build(input_shape, num_outputs, block_fn[num_layers],
+                                     repetitions[num_layers], reg_factor=reg_factor)
