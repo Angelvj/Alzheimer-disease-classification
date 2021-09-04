fe25df78f9840148de78fa4f11f173b4d8ba91ef
Ángel de la Vega Jiménez <32851181+Angelvj@users.noreply.github.com>
2021-09-04 12:05:06 +0200

    lr schedulers

---
+++ code/functions/lr_schedules.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  4 12:00:54 2021
+
+@author: angel
+"""
+
+import tensorflow as tf
+
+def get_lr_decay_callback(lr_max, lr_min, decay, verbose=False):
+    def lrfn(epoch):
+        lr = (lr_max - lr_min) * decay**(epoch) + lr_min
+        return lr
+
+    lr_callback = tf.keras.callbacks.LearningRateScheduler(lrfn, verbose)
+    return lr_callback
+
+
+def get_exp_lr_decay_callback(lr_ini, decay, epochs, verbose=False):
+    def lrfn(epoch):
+        lr = lr_ini * decay**(epoch/epochs)
+        return lr
+
+    lr_callback = tf.keras.callbacks.LearningRateScheduler(lrfn, verbose)
+    return lr_callback
