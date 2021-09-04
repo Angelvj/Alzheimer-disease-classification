9c09104aaf0d028aeea81e8447871da5bf6e437d
Ángel de la Vega Jiménez <32851181+Angelvj@users.noreply.github.com>
2021-09-04 11:46:56 +0200

    added pet feedforward models

---
+++ code/functions/model_evaluation.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  4 11:08:20 2021
+
+@author: angel
+"""
+
---
+++ code/models/feedforward_models_pet.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  4 11:25:13 2021
+
+@author: angel
+"""
+
+import tensorflow as tf
+from tensorflow.keras.layers import MaxPooling3D, Flatten, Dense, Conv3D, BatchNormalization, Input, Dropout
+from tensorflow.keras.regularizers import l2
+
+
+def model_0(input_shape):
+    inputs = tf.keras.layers.Input(input_shape)
+    
+    x = tf.keras.layers.Conv3D(filters=32, kernel_size=5, activation="relu")(inputs)
+    x = MaxPooling3D(pool_size=2)(x)
+    
+    x = Flatten()(x)
+    x = Dense(units=256, activation="relu")(x)
+    
+    outputs = Dense(units=3, activation="softmax")(x)
+   
+    model = tf.keras.Model(inputs, outputs, name="model_0_pet")
+    return model
+
+
+def model_1_pet(input_shape):
+    inputs = tf.keras.layers.Input(input_shape)
+    
+    x = Conv3D(filters=32, kernel_size=5, activation="relu")(inputs)
+    x = MaxPooling3D(pool_size=2)(x)
+    
+    x = Conv3D(filters=32, kernel_size=5, activation="relu")(x)
+    x = MaxPooling3D(pool_size=2)(x)
+    
+    x = Flatten()(x)
+    x = Dense(units=256, activation="relu")(x)
+    outputs = Dense(units=3, activation="softmax")(x)
+    
+    model = tf.keras.Model(inputs, outputs, name="model_1_pet")
+    return model
+
+
+def model_2_pet(input_shape):
+    
+    inputs = tf.keras.layers.Input(input_shape)
+
+    x = Conv3D(filters=16, kernel_size=5, activation="relu")(inputs)
+    x = MaxPooling3D(pool_size=2)(x)
+
+    x = Conv3D(filters=64, kernel_size=5, activation="relu")(x)
+    x = MaxPooling3D(pool_size=2)(x)
+
+    x = Conv3D(filters=128, kernel_size=5, activation="relu")(x)
+    x = MaxPooling3D(pool_size=2)(x)
+
+    x = Flatten()(x)
+    x = Dense(units=256, activation="relu")(x)
+
+    outputs = Dense(units=3, activation="softmax")(x)
+
+    model = tf.keras.Model(inputs, outputs, name="model_2_pet")
+    return model
+
+
+def model_3(input_shape):
+    
+    inputs = tf.keras.layers.Input(input_shape)
+
+    x = Conv3D(filters=16, kernel_size=3, activation='relu')(inputs)
+    x = Conv3D(filters=16, kernel_size=3, activation='relu')(x)
+    x = MaxPooling3D(pool_size=2)(x)
+
+    x = Conv3D(filters=64, kernel_size=3, activation="relu")(x)
+    x = Conv3D(filters=64, kernel_size=3, activation='relu')(x)
+    x = MaxPooling3D(pool_size=2)(x)
+
+    x = BatchNormalization()(x)
+    x = Conv3D(filters=128, kernel_size=3, activation="relu")(x)
+    x = Conv3D(filters=128, kernel_size=3, activation='relu')(x)
+    x = MaxPooling3D(pool_size=2)(x)
+
+    x = Flatten()(x)
+    x = Dropout(rate=0.1)(x)
+    x = Dense(units=256, activation="relu")(x)
+
+    outputs = Dense(units=3, activation="softmax")(x)
+
+    model = tf.keras.Model(inputs, outputs, name="model_3_pet")
+    return model
+
+
+def model_4(input_shape):
+
+    inputs = Input(input_shape)
+
+    x = Conv3D(filters=16, kernel_size=3, activation='relu', kernel_regularizer=l2(0.00005))(inputs)
+    x = Conv3D(filters=16, kernel_size=3, activation='relu', kernel_regularizer=l2(0.00005))(x)
+    x = MaxPooling3D(pool_size=2)(x)
+
+    x = Conv3D(filters=64, kernel_size=3, activation='relu', kernel_regularizer=l2(0.00005))(x)
+    x = Conv3D(filters=64, kernel_size=3, activation='relu', kernel_regularizer=l2(0.00005))(x)
+    x = Conv3D(filters=64, kernel_size=3, activation='relu', kernel_regularizer=l2(0.00005))(x)
+    x = MaxPooling3D(pool_size=2)(x)
+
+    x = BatchNormalization(momentum=0.9)(x)
+    x = Conv3D(filters=128, kernel_size=3, activation='relu', kernel_regularizer=l2(0.00005))(x)
+    x = Conv3D(filters=128, kernel_size=3, activation='relu', kernel_regularizer=l2(0.00005))(x)
+    x = Conv3D(filters=128, kernel_size=3, activation='relu', kernel_regularizer=l2(0.00005))(x)
+    x = MaxPooling3D(pool_size=2, strides=2)(x)
+
+    x = Flatten()(x)
+    x = Dropout(rate=0.2)(x)
+    x = Dense(units=256, activation='relu')(x)
+    x = Dense(units=128, activation='relu')(x)
+
+    outputs = Dense(units=3, activation="softmax")(x)
+
+    model = tf.keras.Model(inputs, outputs, name="model_4_pet")
+    return model
