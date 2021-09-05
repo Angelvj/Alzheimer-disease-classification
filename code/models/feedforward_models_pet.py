d28eebfaf2012b48c8d63d8dcf2309cb907b54c8
Ángel de la Vega Jiménez <32851181+Angelvj@users.noreply.github.com>
2021-09-05 09:48:05 +0200

    added model for pet augmentation

--- code/models/feedforward_models_pet.py
+++ code/models/feedforward_models_pet.py
@@ -119,3 +119,33 @@ def model_4(input_shape):
 
     model = tf.keras.Model(inputs, outputs, name="model_4_pet")
     return model
+
+
+def model_4_augmentation(input_shape):
+
+    inputs = Input(input_shape)
+
+    x = Conv3D(filters=16, kernel_size=3, activation='relu', kernel_regularizer=l2(0.00005))(inputs)
+    x = Conv3D(filters=16, kernel_size=3, activation='relu', kernel_regularizer=l2(0.00005))(x)
+    x = MaxPooling3D(pool_size=2)(x)
+
+    x = Conv3D(filters=64, kernel_size=3, activation='relu', kernel_regularizer=l2(0.00005))(x)
+    x = Conv3D(filters=64, kernel_size=3, activation='relu', kernel_regularizer=l2(0.00005))(x)
+    x = Conv3D(filters=64, kernel_size=3, activation='relu', kernel_regularizer=l2(0.00005))(x)
+    x = MaxPooling3D(pool_size=2)(x)
+
+    x = BatchNormalization(momentum=0.95)(x)
+    x = Conv3D(filters=128, kernel_size=3, activation='relu', kernel_regularizer=l2(0.00005))(x)
+    x = Conv3D(filters=128, kernel_size=3, activation='relu', kernel_regularizer=l2(0.00005))(x)
+    x = Conv3D(filters=128, kernel_size=3, activation='relu', kernel_regularizer=l2(0.00005))(x)
+    x = MaxPooling3D(pool_size=2, strides=2)(x)
+
+    x = Flatten()(x)
+    x = Dropout(rate=0.1)(x)
+    x = Dense(units=256, activation='relu')(x)
+    x = Dense(units=128, activation='relu')(x)
+
+    outputs = Dense(units=3, activation="softmax")(x)
+
+    model = tf.keras.Model(inputs, outputs, name="model_4_pet")
+    return model
